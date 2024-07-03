ee11ec1b8359fa1a2700edbfb76f321d5acf344e
williamlimasilva <williamtakuma@yahoo.com.br>
2024-07-03 10:40:21 -0300

    Improved build.gradle and added spring boot dev tools

---
+++ src/main/java/me/dio/service/implement/UserServiceImplement.java
@@ -0,0 +1,31 @@
+package me.dio.service.implement;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImplement implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImplement(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())){
+            throw new IllegalArgumentException("This Account Number already exists");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
